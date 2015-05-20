7214aae0d66f3260371e1a09f94d748660ebc366
Marion Schleifer <marion.schleifer@gmail.com>
2015-05-20 11:29:34 +0200

    proper formatting

--- turn_table.rb
+++ turn_table.rb
@@ -2,24 +2,24 @@ require_relative 'section'
 
 class TurnTable
 
-	def initialize
-		@open = false
-		@sections = []
-		16.times do
-			@sections << Section.new
-		end
-	end
+  def initialize
+    @open = false
+    @sections = []
+    16.times do
+      @sections << Section.new
+    end
+  end
 
-	def open
-		@open = true
-	end
+  def open
+    @open = true
+  end
 
-	def turnable?
-		if @open == false
-			true
-		else 
-			false
-		end
-	end
+  def turnable?
+    if @open == false
+      true
+    else 
+      false
+    end
+  end
 
 end
