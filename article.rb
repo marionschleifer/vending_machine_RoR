3b10c88bcebac3fc0c5c920ee5356345cf692290
Marion Schleifer <marion.schleifer@gmail.com>
2015-05-21 13:37:52 +0200

    class Article class Section

---
+++ article.rb
@@ -0,0 +1,9 @@
+class Article
+
+  def initialize(name, price, expiry_date)
+    @name = name
+    @price = price
+    @expiry_date = expiry_date
+  end
+
+end
--- section.rb
+++ section.rb
@@ -1,2 +1,38 @@
+require_relative 'article'
+
 class Section
+
+  def initialize
+    @empty = true
+  end
+
+  def empty?
+    @empty
+  end
+
+  def fill_with_article(article)
+    if @empty
+      @article = Article.new(nil)  #hier bin ich nicht sicher, ich muss hier ja eigentlich einen bestimmten Article erstellen.
+      @empty = false
+    else
+      replace_article(article)
+    end
+  end
+
+  def replace_article(article)
+    @article = Article.new(nil)
+  end
 end
+
+#vielleicht waere so etwas noch einfacher:
+
+class Section
+  def initialize
+    @article = nil
+  end
+  def new_article(article)
+    @article = Article.new(nil)
+  end
+end
+
+#wenn es den @article schon gibt, wird er dann einfach ueberschrieben, oder vergesse ich hier was?
